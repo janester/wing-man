1b29cdbde74cad323ab9708e7fe2cbc8cb85144f
Jane Sternbach <janesternbach@gmail.com>
2014-11-17 01:23:03 -0800

    JS window size change and removing id

--- main.js
+++ main.js
@@ -8,11 +8,10 @@ chrome.app.runtime.onLaunched.addListener(function() {
   // Center window on screen.
   var screenWidth = screen.availWidth;
   var screenHeight = screen.availHeight;
-  var width = 600;
-  var height = 900;
+  var width = 500;
+  var height = 600;
 
   chrome.app.window.create('index.html', {
-    id: "helloWorldID",
     outerBounds: {
       width: width,
       height: height,
